50d0c885f56259ca45cfb13cef1097bc65a71d51
David Clark <daclark1@student.fullsail.edu>
2020-01-18 12:46:04 -0600

    Modified Splash activity to transition to Game or Leaderboard activity after a brief delay with a slide transition.  Game or Leaderboard activity replaces the Splash activity so users cannot swipe back to it.

--- app/src/main/java/edu/fullsail/echo/SplashActivity.java
+++ app/src/main/java/edu/fullsail/echo/SplashActivity.java
@@ -1,15 +1,29 @@
 package edu.fullsail.echo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.wearable.activity.WearableActivity;
-import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
 
 public class SplashActivity extends WearableActivity {
     @Override protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_splash );
-
-        // Enables Always-on
         setAmbientEnabled();
+
+        Runnable   transitionSplash  = () -> {
+            startActivity( new Intent( this, GameOrLeaderboardActivity.class ) );
+            overridePendingTransition( android.R.anim.slide_in_left, android.R.anim.slide_out_right );
+            finish();
+        };
+
+        Handler    transitionHandler = new Handler();
+
+        final int  delaySeconds      = getResources().getInteger( R.integer.splash_delay_seconds );
+        final long delayMillis       = TimeUnit.SECONDS.toMillis( delaySeconds );
+
+        transitionHandler.postDelayed( transitionSplash, delayMillis );
     }
 }
