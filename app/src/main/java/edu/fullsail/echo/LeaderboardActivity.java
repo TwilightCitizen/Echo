0b775f22b7589fa879c40e17dcb063a6758ddca3
David Clark <daclark1@student.fullsail.edu>
2020-01-29 20:38:55 -0500

    Added RecyclerView, Adapter, and ViewHolder functionality to Leaderboard activity, along with a quick, rudimentary leaderboard item layout.  The Leaderboard activity now displays leaderboard items when connectivity is available.  Just needs some cleanup, refactoring, and commenting, and some finesse to differentiate the first, second, and third leaders, and the remaining top 10 leaders from the rest.

--- app/src/main/java/edu/fullsail/echo/LeaderboardActivity.java
+++ app/src/main/java/edu/fullsail/echo/LeaderboardActivity.java
@@ -9,9 +9,14 @@ package edu.fullsail.echo;
 
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -22,10 +27,49 @@ obtained by a Firebase database.  Presently, functionality just displays an empt
 navigation could be stubbed out.
 */
 public class LeaderboardActivity extends WearableActivity {
+    private class LeaderAdapter extends RecyclerView.Adapter< LeaderAdapter.LeaderViewHolder > {
+        class LeaderViewHolder extends RecyclerView.ViewHolder {
+            TextView textLeaderScore;
+            TextView textLeaderName;
+
+            LeaderViewHolder( @NonNull View itemView ) {
+                super( itemView );
+
+                textLeaderScore = itemView.findViewById( R.id.textLeaderScore );
+                textLeaderName  = itemView.findViewById( R.id.textLeaderName  );
+            }
+        }
+
+        private ArrayList< EchoLeaderBoardEntry > echoLeaderBoardEntries;
+
+        LeaderAdapter( ArrayList< EchoLeaderBoardEntry > leaderBoardEntries ) {
+            this.echoLeaderBoardEntries = leaderBoardEntries;
+        }
+
+        @NonNull @Override public LeaderViewHolder onCreateViewHolder( @NonNull ViewGroup parent, int viewType ) {
+            View leaderboardItem = LayoutInflater
+                .from( parent.getContext() ).inflate( R.layout.item_leaderboard, parent, false );
+
+            return new LeaderViewHolder( leaderboardItem );
+        }
+
+        @Override public void onBindViewHolder( @NonNull LeaderViewHolder holder, int position ) {
+            EchoLeaderBoardEntry echoLeaderBoardEntry = echoLeaderBoardEntries.get( position );
+
+            holder.textLeaderScore.setText( String.valueOf( echoLeaderBoardEntry.getFinalScore() ) );
+            holder.textLeaderName.setText( echoLeaderBoardEntry.getDisplayName() );
+        }
+
+        @Override public int getItemCount() { return echoLeaderBoardEntries.size(); }
+    }
+
     @Override protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_leaderboard );
+        setupLeaderboard();
+    }
 
+    private void setupLeaderboard() {
         // Get a handle to the retrieving frame.
         FrameLayout frameRetrieving = findViewById( R.id.frameRetrieving );
 
@@ -35,13 +79,24 @@ public class LeaderboardActivity extends WearableActivity {
                 frameRetrieving.setVisibility( View.GONE );
 
                 // Show the retrieved leaderboard or the empty progress.
-                FrameLayout frameLeaders = findViewById( R.id.frameLeaders );
-                FrameLayout frameEmpty   = findViewById( R.id.frameEmpty );
-
-                boolean zeroLimitLeaders = topLimitLeaders.size() < 1;
+                FrameLayout frameLeaders     = findViewById( R.id.frameLeaders );
+                FrameLayout frameEmpty       = findViewById( R.id.frameEmpty );
+                boolean     zeroLimitLeaders = topLimitLeaders.size() < 1;
 
                 frameLeaders.setVisibility( zeroLimitLeaders ? View.GONE    : View.VISIBLE );
                 frameEmpty.setVisibility(   zeroLimitLeaders ? View.VISIBLE : View.GONE    );
+
+                // Guard against setting up an empty recycler view.
+                if( zeroLimitLeaders ) return;
+
+                RecyclerView               recycleLeaders = findViewById( R.id.recycleLeaders );
+                RecyclerView.LayoutManager layoutManager  = new LinearLayoutManager( this );
+                RecyclerView.Adapter       adapterLeaders = new LeaderAdapter( topLimitLeaders );
+
+                recycleLeaders.setHasFixedSize( true );
+                recycleLeaders.setLayoutManager( layoutManager );
+                recycleLeaders.setAdapter( adapterLeaders );
+
             },
 
             ( Exception e ) -> {
@@ -55,4 +110,6 @@ public class LeaderboardActivity extends WearableActivity {
             }
         );
     }
+
+
 }
